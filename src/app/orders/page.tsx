6909136925d8c7a5eec284571d7f285a4506fc95
abhi-saini1 <abhishek977933@gmail.com>
2024-07-23 13:47:05 +0530

    commit

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,23 +1,12 @@
-// import { fetchOrders } from "@/lib/fetchOrders";
-// import UserOrderPage from "@/components/DataTable/UserOrderPage";
+import { fetchOrders } from "@/lib/fetchOrders";
+import UserOrderPage from "@/components/DataTable/UserOrderPage";
 
-// const OrdersPage = async () => {
-//   const recentOrders = await fetchOrders();
+const OrdersPage = async () => {
+  const recentOrders = await fetchOrders();
 
-//   return <UserOrderPage recentOrders={recentOrders} />;
-// };
-
-// export default OrdersPage;
-
-
-const OrdersPage =  () => {
-  // const recentOrders = await fetchOrders();
-
-  return(
-    <>
-      ordartys page
-    </>
-  );
+  return <UserOrderPage recentOrders={recentOrders} />;
 };
 
 export default OrdersPage;
+
+
